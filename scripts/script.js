7e4af6de96d609ed59cdaa9554a69c02ebdc18da
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-10 11:36:52 +0300

    update script

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,1 @@
-<script defer data-domain="jaantollander.github.io/resume" src="https://plausible.io/js/plausible.js"></script>
+<script defer data-domain="jaantollander.github.io" src="https://plausible.io/js/plausible.js"></script>
