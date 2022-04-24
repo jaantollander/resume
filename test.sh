4b501acffeed9f67035636da6ef97a4b4bc8282f
Jaan Tollander de Balsch <jaan@hey.com>
2022-04-24 12:17:51 +0300

    shellcheck netlify github action

--- test.sh
+++ test.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env sh
 shellcheck -s sh ./build.sh -x ./source.sh
+
+# Shellcheck for Netlify GitHub action.
+for i in 1 2 3; do
+    yq ".jobs.build-resume.steps[$i].run" < ".github/workflows/netlify.yml" \
+        | shellcheck -s sh -
+done
